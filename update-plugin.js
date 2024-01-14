b874b08e411595ff2480c247d4d3d70fd6dc7971
AtmoOmen <atmoomen2004@outlook.com>
2024-01-14 13:22:54 +0800

    New workflow

--- update-plugin.js
+++ update-plugin.js
@@ -1,27 +1,32 @@
 const fs = require('fs');
 
 const pluginName = process.env.NAME;
-const version = process.env.VERSION; 
-const downloadUrl = process.env.DOWNLOAD_URL; 
+const downloadCount = process.env.DOWNLOAD_COUNT;
+const version = process.env.VERSION;
+const downloadUrl = process.env.DOWNLOAD_URL;
 const path = 'pluginmaster.json';
 
 const content = fs.readFileSync(path, 'utf8');
 const plugins = JSON.parse(content);
 
+const defaultProperties = {
+    Name: pluginName,
+    DownloadLinkInstall: downloadUrl,
+    DownloadLinkTesting: downloadUrl,
+    DownloadLinkUpdate: downloadUrl,
+    AssemblyVersion: version,
+    DownloadCount: downloadCount
+};
+
 const item = plugins.find(p => p.Name === pluginName);
 if (item) {
-    item.DownloadLinkInstall = downloadUrl;
-    item.DownloadLinkTesting = downloadUrl;
-    item.DownloadLinkUpdate = downloadUrl;
-    item.AssemblyVersion = version;
+    for (const key in defaultProperties) {
+        if (!item.hasOwnProperty(key)) {
+            item[key] = defaultProperties[key];
+        }
+    }
 } else {
-    plugins.push({
-        Name: pluginName,
-        DownloadLinkInstall: downloadUrl,
-        DownloadLinkTesting: downloadUrl,
-        DownloadLinkUpdate: downloadUrl,
-        AssemblyVersion: version
-    });
+    plugins.push(defaultProperties);
 }
 
 fs.writeFileSync(path, JSON.stringify(plugins, null, 2));
