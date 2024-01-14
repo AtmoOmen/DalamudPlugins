d9d297bfd8166095bf824914c373d789221be14c
AtmoOmen <atmoomen2004@outlook.com>
2024-01-14 12:43:29 +0800

    更新 update-plugin.js

--- update-plugin.js
+++ update-plugin.js
@@ -1,27 +1,27 @@
 const fs = require('fs');
 
 const pluginName = process.env.NAME;
-const version = process.env.VERSION;
-const downloadUrl = process.env.DOWNLOAD_URL;
+const version = process.env.VERSION; 
+const downloadUrl = process.env.DOWNLOAD_URL; 
 const path = 'pluginmaster.json';
 
 const content = fs.readFileSync(path, 'utf8');
-const json = JSON.parse(content);
+const plugins = JSON.parse(content);
 
-const item = json.plugins.find(p => p.Name === pluginName);
+const item = plugins.find(p => p.Name === pluginName);
 if (item) {
     item.DownloadLinkInstall = downloadUrl;
     item.DownloadLinkTesting = downloadUrl;
     item.DownloadLinkUpdate = downloadUrl;
-    item.Version = version;
+    item.AssemblyVersion = version;
 } else {
-    json.plugins.push({
+    plugins.push({
         Name: pluginName,
         DownloadLinkInstall: downloadUrl,
         DownloadLinkTesting: downloadUrl,
         DownloadLinkUpdate: downloadUrl,
-        Version: version
+        AssemblyVersion: version
     });
 }
 
-fs.writeFileSync(path, JSON.stringify(json, null, 2));
+fs.writeFileSync(path, JSON.stringify(plugins, null, 2));
